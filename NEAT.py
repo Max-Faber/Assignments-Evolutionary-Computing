198978f7eddd842e644b8490fafcc59ac81d5452
MaxFaber1998 <maxfaber@quicknet.nl>
2021-09-16 09:27:28 +0200

    - Added visualization - Genome fitness is now based on the individual gain (as stated in Assignment I)

--- NEAT.py
+++ NEAT.py
@@ -1,4 +1,4 @@
-import os, sys, neat, inspect, pickle, numpy as np
+import os, sys, neat, visualize, pickle, numpy as np
 from numpy.core.numeric import outer
 sys.path.insert(0, 'evoman') 
 from environment import Environment
@@ -6,6 +6,8 @@ from controller import Controller
 
 experiment_name = 'NEAT'
 ffNetwork       = None
+stats           = None
+highest_fitness = float('-inf')
 
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
@@ -23,54 +25,33 @@ class player_controller(Controller):
         # Normalises the input using min-max scaling
         inputs = (inputs - min(inputs)) / float((max(inputs) - min(inputs)))
         output = ffNetwork.activate(inputs)
-        # takes decisions about sprite actions 
-        if output[0] > 0.5:
-            left = 1
-        else:
-            left = 0
 
-        if output[1] > 0.5:
-            right = 1
-        else:
-            right = 0
-
-        if output[2] > 0.5:
-            jump = 1
-        else:
-            jump = 0
-
-        if output[3] > 0.5:
-            shoot = 1
-        else:
-            shoot = 0
-
-        if output[4] > 0.5:
-            release = 1
-        else:
-            release = 0
-
+        # takes decisions about sprite actions
+        left    = 1 if output[0] > 0 else 0
+        right   = 1 if output[1] > 0 else 0
+        jump    = 1 if output[2] > 0 else 0
+        shoot   = 1 if output[3] > 0 else 0
+        release = 1 if output[4] > 0 else 0
         return [left, right, jump, shoot, release]
 
 generation = 0
 
 def eval_genomes(genomes, config):
-    global ffNetwork, generation
-    nets    = []
-    sim     = 0
-
-
-    for genome_id, genome in genomes:
-        genome.fitness = 0.0
-        nets.append(neat.nn.FeedForwardNetwork.create(genome, config))
-    fit_pop = []
-    for index, y in enumerate(pop):
-        ffNetwork = nets[index]
-        fit_pop.append(simulation(env, y))
-        genomes[index][1].fitness += fit_pop[len(fit_pop) - 1]
-        print('Generation: {}, simulation: {}'.format(generation, sim))
-        sim += 1
+    global ffNetwork, generation, stats, npop, highest_fitness
+    sim             = 0
+    
     generation += 1
-    # f, p, e, t = env.play(pcont=...) # fitness, self.player.life, self.enemy.life, self.time
+    for _, genome in genomes:
+        genome.fitness = 0.0
+        ffNetwork = neat.nn.FeedForwardNetwork.create(genome, config)
+        genome.fitness += simulation(env, None)
+        if genome.fitness > highest_fitness and genome.fitness > 0:
+            highest_fitness = genome.fitness
+            with open('Highscores/highest_genome({}_gain).pk1'.format(highest_fitness), 'wb') as output:
+                pickle.dump(genome, output)
+        sim += 1
+        print('Generation: {}, simulation: {}'.format(generation, sim))
+    visualize.plot_stats(stats, ylog=False, view=False, filename='Graphs/avg_fitness.svg')
 
 env                 = Environment(experiment_name=experiment_name, speed='fastest', playermode='ai',
                                   player_controller=player_controller(), enemymode='static',
@@ -85,10 +66,13 @@ pop                 = np.random.uniform(dom_l, dom_u, (npop, n_vars))
 
 # runs simulation
 def simulation(env, x):
-    f, p, e, t = env.play(pcont=x) # fitness, self.player.life, self.enemy.life, self.time
-    return f
+    # f, p, e, t = env.play(pcont=x) # fitness, self.player.life, self.enemy.life, self.time
+    f, p, e, t = env.play() # fitness, self.player.life, self.enemy.life, self.time
+    # return f
+    return p - e
 
 def run(config_file):
+    global stats
     config   = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                            neat.DefaultSpeciesSet, neat.DefaultStagnation,
                            config_file)
@@ -101,18 +85,21 @@ def run(config_file):
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(5))
 
-    playWithWinningGenome(config)
-    # winner  = p.run(eval_genomes, 50)
+    # playWithWinningGenome(config)
+    winner  = p.run(eval_genomes, 10)
+    visualize.draw_net(config, winner, True, filename='Graphs/network.svg')
+    visualize.plot_stats(stats, ylog=False, view=True, filename='Graphs/avg_fitness.svg')
+    visualize.plot_species(stats, view=True, filename='Graphs/spediation.svg')
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
-    with open('winner.pk1', 'wb') as output:
+    with open('Highscores/winner.pk1', 'wb') as output:
         pickle.dump(winner, output)
 
 def playWithWinningGenome(config):
     global ffNetwork
 
-    with open('winner.pk1', 'rb') as output:
+    with open('Highscores/highest_genome_98_gain.pk1', 'rb') as output:
         genome = pickle.load(output)
     ffNetwork = neat.nn.FeedForwardNetwork.create(genome, config)
     env.speed = 'normal'
