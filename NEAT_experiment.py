9b55e8c5fabad4ca3487469aaffc75e1fdc2ea6e
b0ster <bosterenborg@gmail.com>
2021-09-22 17:33:45 +0200

    Expanded the experiment

--- NEAT_experiment.py
+++ NEAT_experiment.py
@@ -1,16 +1,18 @@
-import sys
+import sys, shutil, os, pickle, numpy
 
 sys.path.insert(0, 'evoman')
 from time import time, strftime, localtime
 from NEAT_evoman import EvomanNEAT
+import NEAT_visualize
 
-number_of_rounds = 1
+number_of_rounds = 3
 experiments = [
     {
         "name": "NEAT-v1",
-        "config-file": "NEAT-configs/config-feedforward-1.txt",
-        "3-enemies": [1, 2, 3],
-        "number-of-generations": 20
+        "neat-config-file": "NEAT-configs/config-feedforward-1.txt",
+        "enemies": [1],
+        "number-of-generations": 10,
+        "best-genome-test-quantity": 5
     }
     # {
     #     "name": "NEAT-v2",
@@ -20,20 +22,60 @@ experiments = [
     # }
 ]
 
-if __name__ == '__main__':
-    dt = strftime("%d_%m_%Y_%H_%M_%S", localtime(time()))
-    for experiment in experiments:
-        for i in range(number_of_rounds):
-            name = experiment["name"] + '-' + dt
-            config = experiment["config-file"]
-            print("Running experiment: {}, config file used: {}".format(name, config))
-            for enemy in experiment["3-enemies"]:
-                env_dir = "NEAT-results/" + name + "/round-" + str(i + 1) + "/enemy-" + str(enemy)
-                num_gens = experiment["number-of-generations"]
 
-                n = EvomanNEAT(neat_config=experiment["config-file"],
-                               number_of_gens=int(num_gens),
-                               experiment_env=env_dir,
+class Experiment:
+
+    def __init__(self, cfg):
+        self.name = "NEAT-results/" + cfg["name"]
+        self.neat_cfg = cfg["neat-config-file"]
+        self.enemies = cfg["enemies"]
+        self.num_gens = cfg["number-of-generations"]
+        self.base_env = self.name + '-' + strftime("%d_%m_%Y_%H_%M_%S", localtime(time()))
+        self.best_genome_test_qt = cfg["best-genome-test-quantity"]
+        self.graphs_dir = self.base_env + '/graphs'
+        os.makedirs(self.base_env)
+        shutil.copyfile(self.neat_cfg, self.base_env + '/neat-config.txt')
+        pass
+
+    def run(self):
+        print("Running experiment: {}, config file used: {}".format(self.name, self.neat_cfg))
+        for enemy in self.enemies:
+            enemy_env = self.base_env + "/enemy-" + str(enemy)
+            os.makedirs(enemy_env)
+            enemy_graphs_env = enemy_env + "/graphs"
+            os.makedirs(enemy_graphs_env)
+            # per round the max per gen [[10, 11, 9, ..., n-rounds], [...], ..., n-gens]
+            max_fitness_per_gen = [[] for _ in range(self.num_gens)]
+            # per round the avg per gen [[10.4, 11.9, 9.9, ..., n-rounds], [...], ..., n-gens]
+            avg_fitness_per_gen = [[] for _ in range(self.num_gens)]
+            best_individual_mean_fitnesses = []  # = avg fitness best genome each round -> [80.2, 72.3, ... n-rounds]
+            for i in range(number_of_rounds):
+                enemy_round_env = enemy_env + "/round-" + str(i + 1)
+
+                n = EvomanNEAT(neat_config=self.neat_cfg,
+                               number_of_gens=int(self.num_gens),
+                               experiment_env=enemy_round_env,
                                enemy=enemy)
 
-                winner = n.run()
+                winner, fitness = n.run()
+                fitnesses = []
+                for _ in range(self.best_genome_test_qt):
+                    fitnesses.append(n.eval_genome(winner, n.neat_config()))
+
+                best_individual_mean_fitnesses.append(sum(fitnesses) / len(fitnesses))
+
+                for j in range(len(n.fitnesses_per_gen)):
+                    gen_fitness = n.fitnesses_per_gen[j]
+                    max_fitness_per_gen[j].append(max(gen_fitness))
+                    avg_fitness_per_gen[j].append(sum(gen_fitness) / len(gen_fitness))
+
+            NEAT_visualize.plot_individual_avg_fitness(self.name,
+                                                       best_individual_mean_fitnesses,
+                                                       filename=enemy_graphs_env + '/best_individual_avg_fitness.svg')
+            NEAT_visualize.plot_fitnesses(avg_fitness_per_gen, max_fitness_per_gen,
+                                          filename=enemy_graphs_env + '/gen_fitnesses.svg')
+
+
+if __name__ == '__main__':
+    for experiment in experiments:
+        Experiment(experiment).run()
